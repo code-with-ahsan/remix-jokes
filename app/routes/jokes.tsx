53981febfde2fc09ca963630ea4f1fd0b384bea9
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2021-12-04 12:21:04 +0100

    initial commit
    initial UI
    installed prisma packages

---
+++ app/routes/jokes.tsx
@@ -0,0 +1,23 @@
+import { Outlet } from "remix";
+import type { LinksFunction } from "remix";
+import stylesUrl from "../styles/jokes.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
+};
+
+export default function JokesRoute() {
+  return (
+    <div>
+      <h1>J🤪KES</h1>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
